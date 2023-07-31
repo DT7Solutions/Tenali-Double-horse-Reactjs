fd890d119ac64195c42bd02e39b4b5c90e2a251c
website-krysta <website.krysta@gmail.com>
2023-07-31 18:12:38 +0530

    fixed changes

--- src/components/sections/home/Socialmedia.js
+++ src/components/sections/home/Socialmedia.js
@@ -7,39 +7,31 @@ import { Link } from 'react-router-dom';
 class Socialmedia extends Component {
     render() {
         return (
-            <div className="subheader   pt-5"  >
-                <div className="container">
-                   
-                  
-               
-        <div className="icon-row"   >
-       <div className="icon-wrapper" >
-        <h2>Follow Us : </h2>
-        </div>
-          <div className="icon-wrapper" >
-          
-            <i className="fab fa-facebook " style={{fontSize:'30px'}}></i>
-           
-          </div>
-          <div className="icon-wrapper">
-          
-            <i className="fab fa-twitter" style={{fontSize:'30px'}}></i>
-          </div>
-          <div className="icon-wrapper">
-           
-            <i className="fab fa-instagram" style={{fontSize:'30px'}}></i>
-          </div>
-          <div className="icon-wrapper">
-            
-            <i className="fab fa-linkedin" style={{fontSize:'30px'}}></i>
-          </div>
-        </div>
+          <div className="subheader subfooter-icons   pt-5">
+            <div className="container">
 
-                       
-                    </div>
+              <div className="icon-row ">
+                <div className="icon-wrapper" >
+                  <h2 className='my-font-style'>Follow Us : </h2>
                 </div>
-      
-           
+                <div className="icon-wrapper" >
+                  <i className="fab fa-facebook-f " style={{ fontSize: '30px' }}></i>
+                 
+                </div>
+               
+                <div className="icon-wrapper">
+                  <i className="fab fa-twitter" style={{ fontSize: '30px' }}></i>
+                </div>
+                <div className="icon-wrapper">
+                  <i className="fab fa-instagram" style={{ fontSize: '30px' }}></i>
+                </div>
+                <div className="icon-wrapper">
+                  <i className="fab fa-linkedin-in" style={{ fontSize: '30px' }}></i>
+                </div>
+              </div>
+
+            </div>
+          </div>
         );
     }
 }
