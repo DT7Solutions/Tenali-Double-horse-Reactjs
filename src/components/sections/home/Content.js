06daabbb84cb79ddb269459ff2322316a0817416
website-krysta <website.krysta@gmail.com>
2023-07-31 11:56:12 +0530

    fixed changes

--- src/components/sections/home/Content.js
+++ src/components/sections/home/Content.js
@@ -1,7 +1,9 @@
 import React, { Component, Fragment } from 'react';
 import Socialmedia from './Socialmedia';
-// import Banner from './Banner';
-import Banner  from '../home/Banner-demo';
+// import Banner from '../home-v3/Banner';
+// import Banner from '../home/Banner';
+import Banner from '../home-v2/Banner';
+// import Banner  from '../home/Banner-demo';
 import Productcards from '../blog-grid/Productcards';
 import Stats from './Stats';
 import Menuitems from '../home-v2/Categories';
