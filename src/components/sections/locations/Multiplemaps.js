f57de69bace8e3cb84b4cee961f86c88c523377c
somesetty manideep <104755035+somesettymanideep@users.noreply.github.com>
2023-08-03 21:25:52 -0700

    changes are doner

--- src/components/sections/locations/Multiplemaps.js
+++ src/components/sections/locations/Multiplemaps.js
@@ -1,4 +1,3 @@
-
 import React, { useState, useEffect } from 'react';
 import { MapContainer, TileLayer, Marker, Popup, Tooltip, useMap } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css'; // Import Leaflet CSS
@@ -9,11 +8,13 @@ const locations = {
     position: [16.268, 80.639],
     name: 'Tenali',
     description: 'Description for Guntur',
+    url :'https://www.google.com/maps/place/Tenali+Double+Horse/@16.268832,80.639928,8z/data=!4m6!3m5!1s0x3a4a062f6663586f:0x47ee4650ea8b815d!8m2!3d16.2688321!4d80.6399277!16s%2Fg%2F11dz0y28f_?hl=en&entry=ttu'
   },
   hyderabad: {
     position: [17.437, 78.414],
     name: 'HYDERABAD',
     description: 'Description for Hyderabad',
+    url :'https://www.google.com/maps?ll=17.437395,78.414571&z=8&t=m&hl=en&gl=IN&mapclient=embed&cid=15603634934683396795'
   },
 };
 
@@ -38,6 +39,8 @@ const MapWithClickableLocations = () => {
 
   return (
     <div >
+       <div className="row ">
+       <div className="col-lg-12">
       <div style={{backgroundColor:'#ED4E53'}}>
         <ul className="ullist controls" style={{ display: 'flex',margin: '0', padding: '0', listStyleType: 'none' }}>
           <li key="guntur" style={{margin: '0'}} className={selectedLocation === 'guntur' ? 'active' : ''} >
@@ -80,22 +83,28 @@ const MapWithClickableLocations = () => {
           maxZoom={16}
         />
         {Object.keys(locations).map((locationKey) => (
-          <LocationMarker key={locationKey} position={locations[locationKey].position} name={locations[locationKey].name}>
+          <LocationMarker key={locationKey} position={locations[locationKey].position} name={locations[locationKey].name} url={locations[locationKey].url}>
             {locationKey === selectedLocation && (
               <Popup>{locations[locationKey].description}</Popup>
             )}
           </LocationMarker>
         ))}
       </MapContainer>
-    </div>
+      </div>
+     </div>
+   </div>
   );
 };
 
-const LocationMarker = ({ position, name, children }) => {
+const LocationMarker = ({ position, name,url }) => {
+  const handleMarkerClick = () => {
+    // Open the URL in a new tab
+    window.open(url, '_blank');
+  };
   return (
-    <Marker position={position} icon={customMarker}>
+    <Marker position={position} icon={customMarker} eventHandlers={{ click: handleMarkerClick }}>
       <Tooltip>{name}</Tooltip>
-      {children}
+      
     </Marker>
   );
 };
