1cdabc33817dffacacfb9aff7cecf113557cad8e
website-krysta <website.krysta@gmail.com>
2023-09-06 16:58:30 +0530

    fixed grate place to work

--- src/components/sections/blog-full/Greatplacetowork.js
+++ src/components/sections/blog-full/Greatplacetowork.js
@@ -9,7 +9,7 @@ class Greatplacetowork extends Component {
               
                     <div className="row ">
                         <div className="col-lg-6 mb-lg-30" style={{display:'flex',justifyContent:'center',alignItems:'center'}}> 
-                        <video width="100%" height="95%" controls poster="/assets/img/misc/great-place-to-work.png">
+                        <video width="100%" height="95%" controls poster={process.env.PUBLIC_URL + "/assets/img/misc/great-place-to-work.png"}>
                           <source src={process.env.PUBLIC_URL + "/assets/video/great-place.mp4"} type="video/mp4" />
                       </video>
                             {/* <img src={process.env.PUBLIC_URL + "/assets/img/misc/great-place-to-work.png"} alt="img" className='shadow  p-1  bg-white rounded' /> */}
