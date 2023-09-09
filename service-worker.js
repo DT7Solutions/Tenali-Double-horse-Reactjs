586aa994842c4f566c1dbb11a19c10e80494e4a8
website-krysta <website.krysta@gmail.com>
2023-09-09 12:31:45 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.9eafaba998e03445f429dd94e6a66039.js"
+  "./precache-manifest.5bb118f6df558292589275019a9936c3.js"
 );
 
 workbox.clientsClaim();
