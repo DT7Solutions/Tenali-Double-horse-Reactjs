17af0188588ac7603020e1b7cce63a3c582b2e10
website-krysta <website.krysta@gmail.com>
2023-09-09 12:20:17 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/wp-content/reactjs/precache-manifest.1666ada26f5a5cc8e25b2f87f9d6a0c2.js"
+  "./precache-manifest.14bd3b982ddd4af9e2a7895cc13a2379.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/wp-content/reactjs/index.html", {
+workbox.routing.registerNavigationRoute("./index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
