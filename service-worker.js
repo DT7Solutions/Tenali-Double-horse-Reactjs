45c3d61944a59cda804f6afa091323204f657286
somesetty manideep <104755035+somesettymanideep@users.noreply.github.com>
2023-09-16 03:36:19 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.5bd3a76721a14a59a84e829feff77c31.js"
+  "./precache-manifest.f8a16afb783493e487ca36519c0156a2.js"
 );
 
 workbox.clientsClaim();
