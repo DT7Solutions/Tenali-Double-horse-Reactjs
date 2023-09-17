7cc3dfe5e3c7b13acfa4f93f4f7f7609f13eb712
website-krysta <website.krysta@gmail.com>
2023-09-17 13:08:35 +0530

    add messanger and fedd back

--- src/components/sections/home-v3/Content.js
+++ src/components/sections/home-v3/Content.js
@@ -1,7 +1,10 @@
 import React, { Component, Fragment } from 'react';
 
-import PortfolioV1 from './PortfolioV1';
+// import PortfolioV1 from './PortfolioV1';
 import PortfolioV2 from './Profile'
+
+import Feedback from '../home/Feedback';
+import Messenger from '../home/Messenger';
 // import AppleFactsSlider from '../home/Newproduct';
 
 class Content extends Component {
@@ -10,6 +13,8 @@ class Content extends Component {
             <Fragment>
               
                <PortfolioV2/>
+               <Feedback/>
+ <Messenger/>
                {/* <AppleFactsSlider/> */}
             </Fragment>
         );
