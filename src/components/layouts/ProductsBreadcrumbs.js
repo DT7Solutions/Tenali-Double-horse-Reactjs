75881d7e620e3234e5ffab21cf118ca86ce1f1f4
somesetty manideep <104755035+somesettymanideep@users.noreply.github.com>
2023-10-06 07:06:34 -0700

    update all

--- src/components/layouts/ProductsBreadcrumbs.js
+++ src/components/layouts/ProductsBreadcrumbs.js
@@ -7,10 +7,10 @@ class ProductsBreadcrumb extends Component {
             <div className="sub-header-breadcrumbs " style={{ backgroundImage: `url(${process.env.PUBLIC_URL}${this.props.breadcrumb.bannerimg})` }}>
                 <div className="container">
                     <div className="subheader-inner">
-                        <h1 className='text-light'>{this.props.breadcrumb.pagename}</h1>
+                        <h1 className='text-light  breadcrumb-mobile'>{this.props.breadcrumb.pagename}</h1>
                         <nav aria-label="breadcrumb">
                             <ol className="breadcrumb">
-                                <li className="breadcrumb-item "><Link to="/">Home</Link></li>
+                                <li className="breadcrumb-item  "><Link to="/">Home</Link></li>
                                 <li className="breadcrumb-item active " aria-current="page">{this.props.breadcrumb.pagename}</li>
                             </ol>
                         </nav>
